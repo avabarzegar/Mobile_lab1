848e1a00b3e58a9cbc41b03eafe5e49f2f020ad9
fateme-barzegar <barzegarfa81@gmail.com>
2024-06-04 10:07:54 -0400

    add images

--- lib/main.dart
+++ lib/main.dart
@@ -80,40 +80,16 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
+      body: const Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(decoration: InputDecoration(labelText: 'Login Name'),),
-            TextField(controller: _passwordController,
-              decoration: InputDecoration(labelText: 'Password'),),
-            ElevatedButton(onPressed: ( ) {
-              String password = _passwordController.text;
-              setState(() {
-                if(password == 'QWERTY123'){
-                  imageSource = 'images/idea.png';
-                }else{
-                  imageSource = 'images/stop.png';
-                }
-              });
-            }, child: Text('Login'),),SizedBox(height: 20.0),Image.asset(imageSource,width: 300.0,height: 300.0,),
-          ],
-        ),
+        child:  Column( crossAxisAlignment: CrossAxisAlignment.center,  children: [
+          Text("BROWSE CATEGORIES", style: TextStyle(fontSize: 30.0),),
+          Text("Not sure about exactly which recipe you're looking for? do a search, or dive into our most popular categories. ", style: TextStyle(fontSize: 20.0)),
+          Text("BY MEAT", style: TextStyle(fontSize: 25.0)),
+          Row(  children: [ Icon(Icons.star), Text("1 stick [8 TBSP] Unsalted butter") ] )
+
+        ]),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
