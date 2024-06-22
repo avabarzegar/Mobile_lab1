54286fdaa6713204177fac52b9dab866be258bd4
fateme-barzegar <barzegarfa81@gmail.com>
2024-06-21 20:30:02 -0400

    WEEK 4 CODES

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Lab 2',
+      title: 'Week 4',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
