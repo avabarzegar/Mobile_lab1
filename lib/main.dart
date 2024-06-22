cb8ffe1d2530bca02be07a6d602ea1003ba8260e
fateme-barzegar <barzegarfa81@gmail.com>
2024-06-21 20:20:07 -0400

    WEEK 4 CODES

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,28 @@
-import 'package:encrypted_shared_preferences/encrypted_shared_preferences.dart';
 import 'package:flutter/material.dart';
-
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:encrypted_shared_preferences/encrypted_shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Lab 2',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Login Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -35,87 +32,161 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  var isChecked = false;
-  late TextEditingController _controller; // late means initialize later, but not null
-  late TextEditingController _controller2;
-  late EncryptedSharedPreferences savedData;
+  late TextEditingController _loginController;
+  late TextEditingController _passwordController;
+  String imageFolder = 'images/question-mark.png';
+  final EncryptedSharedPreferences _storage = EncryptedSharedPreferences();
 
   @override
   void initState() {
-    // initialize object, onloaded in HTML
     super.initState();
-    _controller = TextEditingController();
-    _controller2 = TextEditingController();
-
-    //load the file for SharedPreferences, and pass it in when finished
-
-
-    savedData = EncryptedSharedPreferences(); //constructor is not asynchronous
-    savedData.getString("VariableName").then( (unencryptedString)  {
-      if(unencryptedString != null){
-        _controller.text = unencryptedString;
-      }
-    });
+    _loginController = TextEditingController();
+    _passwordController = TextEditingController();
+    _loadCredentials();
   }
 
   @override
-  void dispose() { //unloading the page
+  void dispose() {
+    _loginController.dispose();
+    _passwordController.dispose();
     super.dispose();
-    _controller.dispose(); //delete memory of _controller
-    _controller2.dispose();
   }
 
+
+  void _checkPassword() {
+    setState(() {
+      if (_passwordController.text == 'QWERTY123') {
+        imageFolder = 'images/idea.png';
+      } else {
+        imageFolder = 'images/stop.png';
+      }
+    });
+    _showSaveCredentialsDialog();
+  }
+
+  Future<void> _showSaveCredentialsDialog() async {
+    bool save = false;
+    await showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Save Data'),
+          content: const Text('Would You Like to Save Your Login Data?'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('No'),
+              onPressed: () {
+                save = false;
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Yes'),
+              onPressed: () {
+                save = true;
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+
+    if (save) {
+      _saveCredentialsIfAvailable();
+    } else {
+      _clearCredentials();
+    }
+  }
+
+  Future<void> _saveCredentialsIfAvailable() async {
+    if (_storage != null) {
+      await _saveCredentials();
+    } else {
+      print("Storage is not initialized!");
+    }
+  }
+
+  Future<void> _saveCredentials() async {
+    // Save credentials to EncryptedSharedPreferences
+    await _storage.setString('username', _loginController.text);
+    await _storage.setString('password', _passwordController.text);
+  }
+
+  Future<void> _clearCredentials() async {
+    // Clear saved credentials from EncryptedSharedPreferences
+    await _storage.remove('username');
+    await _storage.remove('password');
+    // Reset text fields
+    _loginController.clear();
+    _passwordController.clear();
+  }
+
+  Future<void> _loadCredentials() async {
+    String? username = await _storage.getString('username');
+    String? password = await _storage.getString('password');
+
+    if (username != null && password != null && username.isNotEmpty && password.isNotEmpty) {
+      _loginController.text = username;
+      _passwordController.text = password;
+
+      WidgetsBinding.instance!.addPostFrameCallback((_) {
+        final snackBar = SnackBar(
+          content: const Text('Previous login credentials are currently loaded.'),
+          duration: const Duration(seconds: 30),
+          action: SnackBarAction(
+            label: 'Clear saved data',
+            onPressed: () {
+              _clearCredentials();
+            },
+          ),
+        );
+
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Theme.of(context).colorScheme.inversePrimary,title: Text(widget.title)),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('Click on the button below:', style: TextStyle(fontSize:20.0), ),
-            ElevatedButton( onPressed:buttonClicked, child:  Text("Click here")  ),
-
-            TextField(controller: _controller,
-                decoration: InputDecoration(
-                    hintText:"Type here",
-                    border: OutlineInputBorder(),
-                    labelText: "Login name:"
-                )),
-
+            TextField(
+              controller: _loginController,
+              decoration: const InputDecoration(
+                hintText: "Type here",
+                border: OutlineInputBorder(),
+                labelText: "Login name",
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                hintText: "Type here",
+                border: OutlineInputBorder(),
+                labelText: "Password",
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              child: const Text("Login"),
+              onPressed: _checkPassword,
+            ),
+            const SizedBox(height: 20),
+            Image.asset(imageFolder, width: 300, height: 300),
+            const SizedBox(height: 20)
           ],
         ),
       ),
     );
   }
-
-  //This function gets run when you click the button
-  void buttonClicked(){
-
-    showDialog<String>(
-      context: context,
-        builder:(BuildContext ctx )=> AlertDialog(
-          title: const Text('Save data'),
-          content: const Text('Do you want to save your login?'),
-          actions: <Widget>[
-            ElevatedButton(child: Text("Ok"),
-              onPressed: () {
-
-                var userTyped = _controller.value.text;//
-
-                //savedData is EncryptedSharedPreferences
-                savedData.setString("VariableName", userTyped);//variable name
-                Navigator.pop(context);
-              }
-              ,),
-            FilledButton(  child: Text("Cancel"),  onPressed: () {  }, ),
-            OutlinedButton(child: Text("Delete"),      onPressed: () {  },)
-
-          ],
-        ),
-    );
-  }
-
-
 }
